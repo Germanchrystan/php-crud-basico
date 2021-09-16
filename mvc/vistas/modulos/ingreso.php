1cba103a51a8f0fb1fe1965a1fce5673ab282648
Germanchrystan <german.chrystan@vusecurity.com>
2021-09-15 22:52:41 -0300

    Registro y Login

--- mvc/vistas/modulos/ingreso.php
+++ mvc/vistas/modulos/ingreso.php
@@ -2,8 +2,15 @@
 <h1>Ingresar</h1>
 
 <form method="POST" action="">
-	<input type="text" placeholder="Usuario" name="usuario" required>
-	<input type="password" placeholder="Contraseña" name="clave" required>
+	<input type="text" placeholder="Usuario" name="usuarioI" required>
+	<input type="password" placeholder="Contraseña" name="claveI" required>
 	<input type="submit" value="Ingresar">
 
 </form>
+
+
+<?php
+$ingreso = new AdminC();
+$ingreso -> ingresoC();
+
+?>
