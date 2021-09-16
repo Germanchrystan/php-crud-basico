d766649775519aa59b43e0dbd7cab3d3dde01244
Germanchrystan <german.chrystan@vusecurity.com>
2021-09-15 23:57:42 -0300

    Read

--- mvc/controladores/empleadosC.php
+++ mvc/controladores/empleadosC.php
@@ -1,20 +1,45 @@
 <?php 
 
 class EmpleadosC{
+    // Registro de Empleados
     public function RegistrarEmpleadosC(){
-        if(isset($_POST["nombreR"]))
-        
-        $datosC = array(
-            "nombre" => $_POST["nombreR"],
-            "apellido" => $_POST["apellidoR"],
-            "email" => $_POST["emailR"],
-            "puesto" => $_POST["puestoR"],
-            "salario" => $_POST["salario"]
-        );
+        if(isset($_POST["nombreR"])){
 
-        $tablaBD = "empleados";
+            $datosC = array(
+                "nombre" => $_POST["nombreR"],
+                "apellido" => $_POST["apellidoR"],
+                "email" => $_POST["emailR"],
+                "puesto" => $_POST["puestoR"],
+                "salario" => $_POST["salarioR"]
+            );
+    
+            $tablaBD = "empleados";
+    
+            $respuesta = EmpleadosM::RegistrarEmpleadosM($datosC, $tablaBD);
+            if($respuesta == "OK") {
+                header("location:index.php?ruta=empleados");
+            } else {
+                echo "Error";
+            }
+        }
+    }
 
-        $respuesta = EmpleadosM::RegistrarEmpleadosM($datosC, $tablaBD);
+    // Mostrar empleados
+    public function MostrarEmpleadosC($tablaBD){
+        $respuesta = EmpleadosM::MostrarEmpleadosM($tablaBD);
+
+        foreach($respuesta as $key -> $value){
+
+            echo '<tr>
+            <td>'.$value["nombre"].'</td>
+            <td>'.$value["apellido"].'</td>
+            <td>'.$value["email"].'</td>
+            <td>'.$value["puesto"].'</td>
+            <td>$'.$value["salario"].'</td>
+            <td><button>Editar</button></td>
+            <td><button>Borrar</button></td>
+            </tr>'; 
+        }
     }
 }
 
---
+++ mvc/modelos/empleadosM.php
@@ -0,0 +1,41 @@
+<?php 
+
+require_once "conexionBD.php";
+
+class EmpleadosM extends conexionBD {
+
+    //Registrar Empleados
+    static public function RegistrarEmpleadosM($datosC, $tablaBD){
+
+        $pdo = conexionBD::cBD()->prepare(
+            "INSERT INTO $tablaBD 
+            (nombre, apellido, email, puesto, salario) 
+            VALUES (:nombre, :apellido, :email, :puesto, :salario)"
+            );
+        $pdo -> bindParam(":nombre", $datosC["nombre"], PDO::PARAM_STR);
+        $pdo -> bindParam(":apellido", $datosC["apellido"], PDO::PARAM_STR);
+        $pdo -> bindParam(":email", $datosC["email"], PDO::PARAM_STR);
+        $pdo -> bindParam(":puesto", $datosC["puesto"], PDO::PARAM_STR);
+        $pdo -> bindParam(":salario", $datosC["salario"], PDO::PARAM_STR);
+        
+        if($pdo -> execute()){
+            return "OK";
+        } else {
+            return "Error";
+        }
+        $pdo -> close();
+    }
+
+    //Mostrar empleados
+    static public function MostrarEmpleadosM($tablaBD){
+        $pdo = conexionBD::cBD()->prepare("SELECT id, nombre, apellido, email, puesto, salario FROM $tablaBD");
+        $pdo -> execute();
+
+        return $pdo -> fetchAll();
+
+        $pdo -> close();
+    }
+}
+
+
+?>
