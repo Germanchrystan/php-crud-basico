1a303ec9f9f63bda2ff5bd68bdad519d12f0df06
Germanchrystan <german.chrystan@vusecurity.com>
2021-09-16 02:26:36 -0300

    Edit

--- mvc/controladores/empleadosC.php
+++ mvc/controladores/empleadosC.php
@@ -28,7 +28,7 @@ class EmpleadosC{
     public function MostrarEmpleadosC($tablaBD){
         $respuesta = EmpleadosM::MostrarEmpleadosM($tablaBD);
 
-        foreach($respuesta as $key -> $value){
+        foreach($respuesta as $key => $value){
 
             echo '<tr>
             <td>'.$value["nombre"].'</td>
@@ -36,11 +36,58 @@ class EmpleadosC{
             <td>'.$value["email"].'</td>
             <td>'.$value["puesto"].'</td>
             <td>$'.$value["salario"].'</td>
-            <td><button>Editar</button></td>
+            <td><a href="index.php?ruta=editar&id='.$value["id"].'"><button>Editar</button></a></td>
             <td><button>Borrar</button></td>
             </tr>'; 
         }
     }
+    // Editar empleados
+    public function EditarEmpleadoC(){
+        $datosC = $_GET["id"];
+        $tablaBD = "empleados";
+
+        $respuesta = EmpleadosM::EditarEmpleadoM($datosC, $tablaBD);
+
+        echo '
+        <input type="hidden" value="'.$respuesta["id"].'" name="idE">
+        
+        <input type="text" value="'.$respuesta["nombre"].'" placeholder="Nombre" name="nombreE" required>
+
+		<input type="text" value="'.$respuesta["apellido"].'" placeholder="Apellido" name="apellidoE" required>
+
+		<input type="email" value="'.$respuesta["email"].'" placeholder="Email" name="emailE" required>
+
+		<input type="text" value="'.$respuesta["puesto"].'" placeholder="Puesto" name="puestoE" required>
+
+		<input type="text" value="'.$respuesta["salario"].'" placeholder="Salario" name="salarioE" required>
+
+		<input type="submit" value="Actualizar">';
+    }
+
+    //Actualizar Empleados
+    public function ActualizarEmpleadoC(){
+        if(isset($_POST["nombreE"])){
+            $datosC = array(
+                "id" => $_POST["idE"],
+                "nombre" => $_POST["nombreE"],
+                "apellido" => $_POST["apellidoE"],
+                "email" => $_POST["emailE"],
+                "puesto" => $_POST["puestoE"],
+                "salario" => $_POST["salarioE"]
+            );
+
+            $tablaBD = "empleados";
+            $respuesta = EMpleadosM::ActualizarEmpleadoM($datosC, $tablaBD);
+            if($respuesta == "OK") {
+                header("location:index.php?ruta=empleados");
+            } else {
+                echo "Error";
+            }
+
+            $pdo -> close();
+        }
+    }
+
 }
 
 ?>
--- mvc/modelos/empleadosM.php
+++ mvc/modelos/empleadosM.php
@@ -35,6 +35,40 @@ class EmpleadosM extends conexionBD {
 
         $pdo -> close();
     }
+
+    //Editar empleados
+    static public function EditarEmpleadoM($datosC, $tablaBD){
+        $pdo = conexionBD::cBD()->prepare("SELECT id, nombre, apellido, email, puesto, salario FROM $tablaBD WHERE id = :id");
+
+        $pdo -> bindParam(":id", $datosC, PDO::PARAM_INT);
+        $pdo -> execute();
+        return $pdo -> fetch();
+
+        $pdo -> close();
+    }
+
+    //Actualizar empleados
+    static public function ActualizarEmpleadoM($datosC, $tablaBD){
+        $pdo = conexionBD::cBD() -> prepare("UPDATE $tablaBD SET 
+        nombre = :nombre, 
+        apellido = :apellido, 
+        email = :email, 
+        puesto = :puesto, 
+        salario = :salario WHERE id = :id");
+
+        $pdo -> bindParam(":id", $datosC["id"], PDO::PARAM_INT);
+        $pdo -> bindParam(":nombre", $datosC["nombre"], PDO::PARAM_INT);
+        $pdo -> bindParam(":apellido", $datosC["apellido"], PDO::PARAM_INT);
+        $pdo -> bindParam(":email", $datosC["email"], PDO::PARAM_INT);
+        $pdo -> bindParam(":puesto", $datosC["puesto"], PDO::PARAM_INT);
+        $pdo -> bindParam(":salario", $datosC["salario"], PDO::PARAM_INT);
+
+        if($pdo -> execute()){
+            return "OK";
+        }else{
+            return "Error";
+        }
+    }
 }
 
 
